0d7d27a85b817ee7e7c29c71e39b4f445efabd91
Nelson Escalante <nescalante92@gmail.com>
2021-02-24 23:13:03 -0500

    last push

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # weatherdashboard
+
+---------
+
+## Description 
+
+Third-party APIs allow developers to access data/functionality by using requests with specific parameters in a URL. Developers are  tasked with retrieving data from another application's API and using it in in their own context. The task is to build a weather dashboard that will run in the browser, provide a city input's current weather as well as a five-day forcast. Using the OpenWeather API you can retriexe weather data for cities to then be stored in local storage.
+
+-----------
+
+## User Story
+
+As a traveler, I would like to search what the predicted daily weather forcast will be from my current city or any given city I search. I would also like to see a five day forcast from any city I search for.
+
+------------
+
+## Acceptance Criteria
+
+- When any city is searched for then the current and future weather conditions for that city are added to the search history.
+
+- When current city weather conditions are viewed, then the city name, data and weather icons: temperature/humidity/wind-speed/UV are presented.
+
+- When I view the UV Index then a color display is presented to show favorable/moderate/severe conditions. 
+
+- When viewing future city weather conditions, a five-day forcast displays the date/weather-icon/temperature/humidity. 
+
+- When a city search history is clicked, then current and future conditions for that city are presented again.
+
+-------------
+
+## Links
+
+- [Link to Github Profile] (https://github.com/nescalante92/weatherdashboard)
+
+- [Link to Deployed Website] (https://nescalante92.github.io/weatherdashboard/)
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //8f220b5e670876424e7610149c0ca000
 var appKey = '8f220b5e670876424e7610149c0ca000';
-var city = 'charlotte'
 var unit = 'imperial';
 //forcast display 
-var cityInput = $('#cityInput');
+var cityInput = $('#cityInput').val();
 var temp = $('#temp');
 var hum = $('#hum');
 var wind = $('#wind');
@@ -16,43 +15,63 @@ $("#currentDay").text(currentDay.format("[Today is ]dddd, MMMM Do, YYYY h:mm:ss 
 /*toString() returns string object value*/
 console.log(currentDay.toString());
 
+$("#saveBtn").on("click", function (event) {
+    // event.preventDefault();
+    inputVal = cityInput.value;
+    console.log($(this).text());
+});
+// saveBtn
+
 
 
 //The geolocation.getCurrentPosition() method is used to get the current position of the device(latitude/longitude) = https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
 // navigator object contains information about the browser like geolocation to locate user's position = https://www.w3schools.com/jsref/obj_navigator.asp
 function weatherForcast() {
     navigator.geolocation.getCurrentPosition(function (position) {
-        longitude = position.coords.longitude;
-        latitude = position.coords.latitude;
+        var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=' + appKey + '&units=' + unit;
 
-        //link to weather forcast api 
-        var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.valueOf() + latitude + '&appid=' + longitude + '&units=' + appKey + unit;
+        console.log(cityURL);
 
-        //fetch function grabs cityURL from web server
-        fetch(cityUrl)
-            //waits for responce from fetched URL
-            .then(function (response) {
-                return response.json();
-            })
-            //waits for responce from fetched json to store/transport data
-            //responce stores retrieved data
-            .then(function (data) {
-                console.log(data);
 
-                cityInput.text(data.name + clock.format("MM/DD/YYYY"));
-                temp.text(data.main.temp);
-                hum.text(data.main.hum);
-                wind.text(data.main.wind);
-                uv.text(data.main.uv);
+    })
 
-            });
-    });
 
+    //link to weather forcast api 
+    //fetch function grabs cityURL from web server
+    fetch(cityUrl)
+        //waits for responce from fetched URL
+        .then(function (response) {
+            return response.json();
+        })
+        //waits for responce from fetched json to store/transport data
+        //responce stores retrieved data
+        .then(function (data) {
+            console.log(data);
+            http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+            var uvURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + '8f220b5e670876424e7610149c0ca000';
+
+            var lon = position.coords.longitude;
+            var lat = position.coords.latitude;
+
+            cityInput.text(data.name);
+            temp.text(data.main.temp);
+            hum.text(data.main.hum);
+            wind.text(data.main.wind);
+            uv.text(data.main.uv);
+
+            var tempEl = $("#temp");
+            var humEl = $("#hum");
+            var windEl = $("#wind");
+            var uvEl = $("#uv");
+        });
 };
+('#thwuvCard')
 
 
-$('#saveBtn').on('click', function (event) {
-    event.preventDefault();
-    weatherForcast();
-})
+
+
+
+
+
+
 
